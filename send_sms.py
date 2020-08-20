79d6df42c6557731edad9683a2b6fddc2d1eaecc
apthagowda97 <iamuraptha@gmail.com>
2020-08-20 19:51:42 +0530

    reformated the code using black

--- send_sms.py
+++ send_sms.py
@@ -7,16 +7,20 @@ from twilio.http.http_client import TwilioHttpClient
 
 load_dotenv()
 
-proxy_client = TwilioHttpClient(proxy={'http': os.getenv("HTTP_PROXY"), 'https': os.getenv("HTTPS_PROXY")})
+proxy_client = TwilioHttpClient(
+    proxy={"http": os.getenv("HTTP_PROXY"), "https": os.getenv("HTTPS_PROXY")}
+)
 client = Client(http_client=proxy_client)
 
-def send_sms(body:str):
+
+def send_sms(body: str):
     """Send the SMS with given message body."""
-    
+
     from_phone = os.getenv("TWILIO_SMS_FROM")
     to_phone = os.getenv("TWILIO_SMS_TO")
-    client.messages.create(from_=from_phone,body=body,to=to_phone)
+    client.messages.create(from_=from_phone, body=body, to=to_phone)
 
-if __name__=='__main__':
-    body = areca_price.get_body(city='SHIVAMOGGA')
+
+if __name__ == "__main__":
+    body = areca_price.get_body(city="SHIVAMOGGA")
     send_sms(body=body)
